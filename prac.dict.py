ce94d22fd48dee70670cec0319691d04a26edc6e
ayushmaan <aayushmaan674@gmail.com>
2025-11-20 21:55:02 +0530

    Delete prac.dict.py

--- prac.dict.py
+++
@@ -1,7 +0,0 @@
-prices={'burger':60,'pizza':100,'pasta':120}
-print(prices,type(prices))
-print(prices.values())
-print(prices.keys())
-prices['burger']=90
-print(prices.pop('pizza'))
-print(prices)
