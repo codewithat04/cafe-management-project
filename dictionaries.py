15a88e7c28b271c26d758fdb3f2c53add384e455
ayushmaan <aayushmaan674@gmail.com>
2025-11-20 21:51:51 +0530

    Delete dictionaries.py

--- dictionaries.py
+++
@@ -1,9 +0,0 @@
-runs = {"virat": 29,"rohit": 68,"hardik":45}
-print(runs, type(runs))
-print(runs["hardik"])
-runs["virat"] = 89#it is changeble.
-print(runs.keys())#this will give you the keys.
-print(runs.values())#this will give you values.
-runs.clear
-runs.pop("virat")#it will remove virat
-print(runs)
